7cf2cdba5e5eadeee723389c4a1b0620a5a3991b
victorbush <victorbush@gmail.com>
2025-03-19 22:43:11 -0500

    ww3d2: fix compilation.

--- GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2/animatedsoundmgr.cpp
+++ GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2/animatedsoundmgr.cpp
@@ -157,7 +157,7 @@ Build_List_From_String
 				// Copy this entry into its own string
 				//
 				StringClass entry_string = entry;
-				char *delim_start = ::strstr (entry_string, delimiter);				
+				char *delim_start = (char*)::strstr(entry_string, delimiter);
 				if (delim_start != NULL) {
 					delim_start[0] = 0;
 				}

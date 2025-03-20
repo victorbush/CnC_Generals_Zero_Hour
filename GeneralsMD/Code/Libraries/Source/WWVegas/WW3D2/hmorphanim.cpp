67b846d2391c7c0df9c8a4e856d8a59326db22b5
victorbush <victorbush@gmail.com>
2025-03-19 22:37:35 -0500

    ww3d2: address compilation issues except for MSS.

--- GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2/hmorphanim.cpp
+++ GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2/hmorphanim.cpp
@@ -295,7 +295,7 @@ static int Build_List_From_String
 				// Copy this entry into its own string
 				//
 				StringClass entry_string = entry;
-				char *delim_start = ::strstr (entry_string, delimiter);				
+				char *delim_start = (char*)::strstr(entry_string, delimiter);
 				if (delim_start != NULL) {
 					delim_start[0] = 0;
 				}
@@ -489,7 +489,7 @@ void HMorphAnimClass::Set_Name(const char * name)
 	// Try to find the separator (a period)
 	//
 	StringClass full_name	= name;
-	char *separator			= ::strchr (full_name, '.');
+	char *separator			= (char*)::strchr(full_name, '.');
 	if (separator != NULL) {
 		
 		//
